02ef7bab8ebe68a70bdc2ec305d79df72c65550b
arielmirra <amirra@mulesoft.com>
2022-03-22 12:36:43 -0300

    chore: organize code into different files

--- index.js
+++ index.js
@@ -3,43 +3,8 @@
 /* eslint-disable no-unused-vars */
 const { ApolloServer, gql } = require('apollo-server')
 const SessionsAPI = require('./datasources/sessions')
-
-const typeDefs = gql`
-type Query {
-    sessions(
-      id: ID
-      title: String,
-      description: String,
-      startsAt: String,
-      endsAt: String,
-      room: String,
-      day: String,
-      format: String,
-      track: String
-      level: String
-    ): [Session],
-    sessionById(id: ID): Session
-}
-type Session {
-    id: ID!
-    title: String,
-    description: String,
-    startsAt: String,
-    endsAt: String,
-    room: String,
-    day: String,
-    format: String,
-    track: String @deprecated(reason: "Too many sessions don't fit under a single track, will become a list"),
-    level: String
-}
-`
-
-const resolvers = {
-  Query: {
-    sessions: (parent, args, { dataSources }, info) => dataSources.sessionAPI.getSessions(args),
-    sessionById: (parent, { id }, { dataSources }, info) => dataSources.sessionAPI.getSessionById(id),
-  },
-}
+const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
 
 const dataSources = () => ({
   sessionAPI: new SessionsAPI(),
---
+++ resolvers.js
@@ -0,0 +1,6 @@
+module.exports = {
+  Query: {
+    sessions: (parent, args, { dataSources }, info) => dataSources.sessionAPI.getSessions(args),
+    sessionById: (parent, { id }, { dataSources }, info) => dataSources.sessionAPI.getSessionById(id),
+  },
+}
